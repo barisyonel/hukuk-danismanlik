a1197f0277063922d3c3470b76930bfa1e83b2cf
Barış Can Yönel <138584541+barisyonel@users.noreply.github.com>
2024-11-28 14:16:14 +0300

    CSS sorunlarını düzelt

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {},
+  base: '/hukuk-danismanlik/',
+  build: {
+    rollupOptions: {
+      external: [
+        '/hukuk-danismanlik/src/main.tsx', // Rollup'un hata aldığı yolu buraya ekliyoruz
+      ],
     },
   },
 });
